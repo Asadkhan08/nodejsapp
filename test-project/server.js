19976a6709db9abcdeb41f35472f2d9555d6983f
Asad <asad@Asad-Laptop>
2024-04-28 17:11:28 +0400

    new changes made to re-route traffic to nodejs from nginx

--- test-project/server.js
+++ test-project/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3002;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send({ message: 'this message is coming from another container having v2! ' });
